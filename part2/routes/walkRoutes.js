f83c323747f85242ba90e6daed20259e1457a77e
DuyDo24 <a1921156@adelaide.edu.au>
2025-06-21 02:23:12 +0000

    [automatic commit] 2025-06-21 02:23:12 UTC 175.35.53.9

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -59,16 +59,4 @@ router.post('/:id/apply', async (req, res) => {
   }
 });
 
-//Logout
-router.get('/logout', (req, res) => {
-  req.session.destroy(err => {
-    if (err) {
-      console.error('Error destroying session:', err);
-      return res.status(500).send('Logout failed.');
-    }
-    res.clearCookie('connect.sid'); // remove session cookie
-    res.redirect('/'); // redirect back to login page
-  });
-});
-
 module.exports = router;
